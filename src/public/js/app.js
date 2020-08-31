57d0db32d3379267a1d7f384b7a12e2fbdf0966d
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-08-31 12:12:48 -0400

    Remove console.log call

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,3 @@
-console.log("Client side javascript is loaded!");
 const weatherForm = document.querySelector("form");
 const searchElement = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
